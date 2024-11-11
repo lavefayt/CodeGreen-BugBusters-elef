4ee074a4d0d1ddafdc7e81dae1e3121c0f7f39f5
xiaokjxiao <krystaljane.siaotong-23@cpu.edu.ph>
2024-11-11 21:45:09 +0800

    fpw no enter new pw

--- src/pages/Forgot.tsx
+++ src/pages/Forgot.tsx
@@ -8,11 +8,13 @@ const Forgot = () => {
     };
 
     return (
-        <div className="flex justify-center items-center bg-secondgrey bg-no-repeat sm:bg-top md:bg-right lg:bg-left h-screen">
-            
-            <div className='bg-hoverbutton p-3 rounded-sm'>
-                <h3 className="font-skye-regular text-white">Forgot Password?</h3>
-                <h5 className="font-skye-regular text-white">Enter the email associated with your account and we'll send an email with a link to reset your password.</h5>
+        <div className="flex justify-center items-center bg-hoverbutton bg-no-repeat sm:bg-top md:bg-right lg:bg-left h-screen">
+           
+            <div className='bg-hoverbutton p-5 rounded-sm w-'>
+                <div className='flex flex-col justify-center items-start font-syke-medium text-textgreen text-4xl gap-2'>
+                    <h1>Reset Password</h1>
+                </div>
+                <h5 className="font-skye-regular text-white mt-2">Please enter your email address to request a password reset.</h5>
                
                
                 <form className="space-y-2" onSubmit={handleSubmit}>
@@ -23,11 +25,15 @@ const Forgot = () => {
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="bg-secondgrey mt-4 font-syke-regular w-full mt-1 px-4 py-2 focus:shadow-inner border-none focus:outline-none focus:ring-1 focus:ring-textgreen rounded-sm text-white placeholder-white"
+                className="bg-secondgrey mt-4 font-syke-regular w-11/12 px-3 py-2 focus:shadow-inner border-none focus:outline-none focus:ring-1 focus:ring-textgreen rounded-sm text-white placeholder-white"
                 placeholder="Email"
                 required/>
-              </div></form> </div>
-        </div>
+              </div></form>
+              <h1 className="mt-4 mb-2 text-buttongreen cursor-pointer font-syke-medium text-sm"
+                onClick={handleSubmit}
+                >Send Reset Email</h1>
+            </div>
+              </div>
     );
 }
 
