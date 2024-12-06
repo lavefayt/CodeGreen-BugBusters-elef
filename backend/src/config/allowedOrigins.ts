8415157c4d7d475aa824f415021d758177be5702
T-Tops <154719134+T-Rops@users.noreply.github.com>
2024-12-06 10:25:27 +0800

    Update allowedOrigins.ts

--- backend/src/config/allowedOrigins.ts
+++ backend/src/config/allowedOrigins.ts
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5175",
   "https://codegreen.up.railway.app:5173",
+  "https://codegreen.up.railway.app:8080",
+  "https://codegreen.up.railway.app",
 ];
 
 export default allowedOrigins;
