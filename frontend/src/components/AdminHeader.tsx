3410d754671cfb7c92a48ddc87532ecb6f5bcaa5
T-Tops <154719134+T-Rops@users.noreply.github.com>
2024-12-08 00:19:07 +0800

    Complete Change Password

--- frontend/src/components/AdminHeader.tsx
+++ frontend/src/components/AdminHeader.tsx
@@ -37,6 +37,10 @@ const AdminHeader = () => {
     setDropdownOpenList((prevState) => !prevState);
   };
 
+  const handleChangePassword = () => {
+    navigate("/changepassword");
+  };
+
   const handleLogOut = async () => {
     logout();
   };
@@ -68,8 +72,7 @@ const AdminHeader = () => {
         <div className="flex items-center w-[15rem] mr-[150px]">
           <button
             onClick={handleHomePage}
-            className="flex text-left items-center gap-4 group overflow-hidden rounded-md pr-20 py-2 text-white font-medium text-lg"
-          >
+            className="flex text-left items-center gap-4 group overflow-hidden rounded-md pr-20 py-2 text-white font-medium text-lg">
             <img
               src="../assets/5.png"
               alt="Logo"
@@ -83,36 +86,36 @@ const AdminHeader = () => {
 
         {/* Navigation Section */}
         <nav className="flex space-x-[8rem] text-white font-syke-medium text-lg">
-          <Link to="/encode" className="hover:text-textgreen transition-colors">
+          <Link
+            to="/encode"
+            className="hover:text-textgreen transition-colors">
             Encode
           </Link>
 
           {/* List Dropdown */}
-          <div className="relative" ref={dropdownRefList}>
+          <div
+            className="relative"
+            ref={dropdownRefList}>
             <button
               onClick={toggleDropdownList}
-              className="hover:text-buttongreen transition-colors"
-            >
+              className="hover:text-buttongreen transition-colors">
               Lists
             </button>
             {isDropdownOpenList && (
               <div className="absolute right-0 mt-4 w-48 bg-hoverbutton text-white rounded-md shadow-lg z-10">
                 <span
                   onClick={handleViolators}
-                  className="block font-syke-medium rounded-t-lg text-sm px-4 py-2 hover:bg-buttongreen cursor-pointer"
-                >
+                  className="block font-syke-medium rounded-t-lg text-sm px-4 py-2 hover:bg-buttongreen cursor-pointer">
                   Violator's List
                 </span>
                 <span
                   onClick={handleDrivers}
-                  className="block font-syke-medium text-sm px-4 py-2 hover:bg-buttongreen cursor-pointer"
-                >
+                  className="block font-syke-medium text-sm px-4 py-2 hover:bg-buttongreen cursor-pointer">
                   Driver's List
                 </span>
                 <span
                   onClick={handleRegistrations}
-                  className="block font-syke-medium text-sm rounded-b-lg px-4 py-2 hover:bg-buttongreen cursor-pointer"
-                >
+                  className="block font-syke-medium text-sm rounded-b-lg px-4 py-2 hover:bg-buttongreen cursor-pointer">
                   Registration's List
                 </span>
               </div>
@@ -120,22 +123,24 @@ const AdminHeader = () => {
           </div>
 
           {/* Account Dropdown */}
-          <div className="relative" ref={dropdownRefAccount}>
+          <div
+            className="relative"
+            ref={dropdownRefAccount}>
             <button
               onClick={toggleDropdownAccount}
-              className="hover:text-buttongreen transition-colors"
-            >
+              className="hover:text-buttongreen transition-colors">
               Account
             </button>
             {isDropdownOpenAccount && (
               <div className="absolute right-0 mt-4 w-48 bg-hoverbutton text-white rounded-md shadow-lg z-10">
-                <span className="block font-syke-medium rounded-t-lg text-sm px-4 py-2 hover:bg-buttongreen cursor-pointer">
-                  Reset Password
+                <span
+                  className="block font-syke-medium rounded-t-lg text-sm px-4 py-2 hover:bg-buttongreen cursor-pointer"
+                  onClick={handleChangePassword}>
+                  Change Password
                 </span>
                 <span
                   onClick={handleLogOut}
-                  className="block font-syke-medium text-sm rounded-b-lg px-4 py-2 hover:bg-buttongreen cursor-pointer"
-                >
+                  className="block font-syke-medium text-sm rounded-b-lg px-4 py-2 hover:bg-buttongreen cursor-pointer">
                   Log Out
                 </span>
               </div>
--- frontend/src/pages/ChangePassword.tsx
+++ frontend/src/pages/ChangePassword.tsx
@@ -1,86 +1,105 @@
 import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
+import useChangePassword from "../hooks/useChangePassword";
+import { useState } from "react";
+import { Spinner } from "react-activity";
+import useAuth from "../hooks/context-hooks/useAuth";
+import { AuthContextType } from "../types/user.types";
 const ChangePassword = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { loading, changePassword } = useChangePassword();
+  const [data, setData] = useState({
+    currentPassword: "",
+    newPassword: "",
+    confirmNewPassword: "",
+  });
+  const { auth }: AuthContextType = useAuth();
 
-    const handleCancelButton = () => { 
-        navigate("/homepage")
-    }
+  const handleCancelButton = () => {
+    navigate(auth?.isAdmin ? "/admin" : "/homepage");
+  };
 
-    const handleSubmitButton = () => { 
-        //pass
-    }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmitButton = () => {
+    changePassword(data);
+  };
 
   return (
     <div className="flex flex-col items-center bg-policies-bg bg-cover bg-no-repeat sm:bg-top md:bg-right lg:bg-left h-screen">
-
       <div>
         <Header />
       </div>
 
       <div className="space-y-5 w-full max-w-md mt-8">
-
         <div className="flex flex-col space-y-4">
-
           <div className="flex-1">
-            <h1 
-            className="text-white font-syke-light text-xl">
-                Current Password
-                </h1>
+            <h1 className="text-white font-syke-light text-xl">
+              Current Password
+            </h1>
             <input
               type="password"
               className="bg-secondgrey border-b font-syke-regular text-[1.2rem] w-full mt-1 px-4 py-2 h-10 border-none focus:outline-none focus:shadow-inner focus:ring-1 focus:ring-textgreen text-white placeholder-white placeholder-opacity-25 rounded-sm"
-              name="current_password"
+              name="currentPassword"
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="flex-1">
+            <h1 className="text-white font-syke-light text-xl">New Password</h1>
+            <input
+              type="password"
+              className="bg-secondgrey border-b font-syke-regular text-[1.2rem] w-full mt-1 px-4 py-2 h-10 border-none focus:outline-none focus:shadow-inner focus:ring-1 focus:ring-textgreen text-white placeholder-white placeholder-opacity-25 rounded-sm"
+              name="newPassword"
+              onChange={handleChange}
               required
             />
           </div>
 
           <div className="flex-1">
             <h1 className="text-white font-syke-light text-xl">
-                New Password
-                </h1>
+              Confirm Password
+            </h1>
             <input
               type="password"
               className="bg-secondgrey border-b font-syke-regular text-[1.2rem] w-full mt-1 px-4 py-2 h-10 border-none focus:outline-none focus:shadow-inner focus:ring-1 focus:ring-textgreen text-white placeholder-white placeholder-opacity-25 rounded-sm"
-              name="new_password"
+              name="confirmNewPassword"
+              onChange={handleChange}
               required
             />
           </div>
-
-          <div className="flex-1">
-            <h1 className="text-white font-syke-light text-xl">
-                Confirm Password
-                </h1>
-            <input
-              type="password"
-              className="bg-secondgrey border-b font-syke-regular text-[1.2rem] w-full mt-1 px-4 py-2 h-10 border-none focus:outline-none focus:shadow-inner focus:ring-1 focus:ring-textgreen text-white placeholder-white placeholder-opacity-25 rounded-sm"
-              name="confirm_password"
-              required
-            />
-          </div>
-
         </div>
-
       </div>
-      
-        <div className="flex justify-between w-full max-w-sm p-5">
 
-            <button
-                type="button"
-                onClick={ handleCancelButton }
-                className="w-32 bg-buttongreen font-syke-medium text-white py-2 hover:bg-[#33471a] font-syke-regular transition-colors rounded-sm">
-                Cancel
-            </button>
+      <div className="flex justify-between w-full max-w-sm p-5">
+        <button
+          type="button"
+          onClick={handleCancelButton}
+          className="w-32 bg-buttongreen font-syke-medium text-white py-2 hover:bg-[#33471a] font-syke-regular transition-colors rounded-sm">
+          Cancel
+        </button>
 
-            <button
-                type="button"
-                onClick={ handleSubmitButton }
-                className="w-32 bg-buttongreen font-syke-medium text-white py-2 hover:bg-[#33471a] font-syke-regular transition-colors rounded-sm">
-                Confirm
-            </button>
-
-        </div>
-
+        <button
+          type="button"
+          onClick={handleSubmitButton}
+          className="w-32 bg-buttongreen font-syke-medium text-white py-2 hover:bg-[#33471a] font-syke-regular transition-colors rounded-sm">
+          {loading ? (
+            <div className="flex justify-center items-center w-full h-full">
+              <Spinner
+                size={10}
+                color="#008000"
+                animating={loading}
+              />
+            </div>
+          ) : (
+            "Confirm"
+          )}
+        </button>
+      </div>
     </div>
   );
 };
