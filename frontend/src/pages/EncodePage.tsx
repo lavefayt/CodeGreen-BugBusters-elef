f3a006357732b9dd0bb24d763d0f24322f21b075
xiaokjxiao <krystaljane.siaotong-23@cpu.edu.ph>
2024-12-20 15:13:31 +0800

    admin and encode responsiveness + register driver fixed

--- frontend/src/pages/EncodePage.tsx
+++ frontend/src/pages/EncodePage.tsx
@@ -13,26 +13,29 @@ const EncodePage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center bg-adminlanding-bg bg-cover bg-no-repeat sm:bg-top md:bg-right lg:bg-left h-screen">
-      <div>
+    <div className="flex flex-col items-center bg-adminlanding-bg bg-cover bg-no-repeat h-screen sm:bg-top md:bg-right lg:bg-left">
+      <div className="w-full">
         <AdminHeader />
       </div>
-      <h1 className="text-white font-syke-light text-3xl mt-5">
+      <h1 className="text-white font-syke-light text-2xl mt-5 text-center sm:text-3xl md:text-4xl lg:text-5xl">
         What would you like to do?
       </h1>
-      <div className="flex justify-center gap-10 p-3 mt-10">
+      <div className="flex flex-col sm:flex-row justify-center gap-6 sm:gap-10 p-4 mt-10">
         <div
-          className="flex items-center justify-center border-5 border-white w-72 h-40 rounded-lg border-2cursor-pointer hover:border-textgreen hover:border-4 transition-all bg-secondgrey text-buttongreen hover:text-textgreen"
-          onClick={handleAddDriverButton}>
-          <span className="font-syke-medium text-white text-3xl">
+          className="flex items-center justify-center w-full sm:w-64 lg:w-72 h-36 sm:h-40 rounded-lg border-2 border-white cursor-pointer hover:border-textgreen hover:border-4 transition-all bg-secondgrey text-buttongreen hover:text-textgreen"
+          onClick={handleAddDriverButton}
+        >
+          <span className="font-syke-medium p-5 text-lg sm:text-xl lg:text-2xl text-white">
             Add Driver
           </span>
         </div>
 
+        {/* Add Violation Button */}
         <div
-          className="flex items-center justify-center w-72 h-40 rounded-lg border-2cursor-pointer border-white hover:border-textgreen hover:border-4 transition-all bg-secondgrey text-buttongreen hover:text-textgreen"
-          onClick={handleAddViolationButton}>
-          <span className="font-syke-medium text-white text-3xl">
+          className="flex items-center justify-center w-full sm:w-64 lg:w-72 h-36 sm:h-40 rounded-lg border-2 border-white cursor-pointer hover:border-textgreen hover:border-4 transition-all bg-secondgrey text-buttongreen hover:text-textgreen"
+          onClick={handleAddViolationButton}
+        >
+          <span className="font-syke-medium p-5 text-lg sm:text-xl lg:text-2xl text-white">
             Add Violation
           </span>
         </div>
